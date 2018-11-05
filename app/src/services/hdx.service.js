e15ae9069dbf780191d8b54d3016217ecafeba27
manuelnelson <elnels@gmail.com>
2018-11-05 13:21:47 -0500

    additional logging

--- app/src/services/hdx.service.js
+++ app/src/services/hdx.service.js
@@ -87,6 +87,8 @@ class HDXIndexService {
             }
 
             if (!hdxResource) {
+                logger.error(`${config.hdx.package}`.replace(':package-id', dataset.tableName))
+                logger.error(hdxPackageResponse.result)
                 throw new Error(`No single JSON or CSV resource found for HDX package ${dataset.tableName}`);
             }
 
