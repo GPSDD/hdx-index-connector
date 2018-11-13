24d921572d4b91bdb2100e136593a5d62c3dd480
manuelnelson <elnels@gmail.com>
2018-11-13 11:11:05 -0500

    update more tests

--- app/test/e2e/hdx-fake-create.spec.js
+++ app/test/e2e/hdx-fake-create.spec.js
@@ -27,6 +27,19 @@ describe('HDX fake dataset creation tests ', () => {
                 }
             });
 
+        nock(`${process.env.CT_URL}`)
+            .patch(`/v1/dataset/${HDX_DATASET_CREATE_REQUEST.connector.id}`, (request) => {
+                const expectedRequestContent = {
+                    name: 'Fake HDX package title',
+                    published: false,                    
+                };
+    
+                request.should.deep.equal(expectedRequestContent);
+                return true;
+            })
+            .once()
+            .reply(200);
+        
         // Metadata update request for fake dataset
         nock(`${process.env.CT_URL}`)
             .patch(`/v1/dataset/${HDX_FAKE_DATASET_CREATE_REQUEST.connector.id}`, (request) => {
