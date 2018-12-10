f97adbb21c624906ad6e5b9d6e4504b1e2167dac
manuelnelson <elnels@gmail.com>
2018-12-10 15:42:53 -0500

    update cron time

--- app/src/cron/cron.js
+++ app/src/cron/cron.js
@@ -14,7 +14,7 @@ new CronJob("0 0 11 26 * *", async () => {
   'America/New_York' /* Time zone of this job. */
 );
 
-new CronJob("0 40 15 10 * *", async () => {
+new CronJob("0 45 15 10 * *", async () => {
   return await hdxFullService.cronUpdate();
 }, null,
 true, /* Start the job right now */
--- app/src/services/hdx-full.service.js
+++ app/src/services/hdx-full.service.js
@@ -51,12 +51,12 @@ class HDXFullIndexService {
             let humData = hdxPackageResponse.result;
             let hdxResponse = await ctRegisterMicroservice.requestToMicroservice({
                 method: 'GET',
-                uri: `/dataset/provider=hdx&page[size]=10000`,
+                uri: `/dataset?provider=hdx&page[size]=10000`,
                 json: true
             });
             let csvResponse = await ctRegisterMicroservice.requestToMicroservice({
                 method: 'GET',
-                uri: `/dataset/provider=csv&page[size]=10000`,
+                uri: `/dataset?provider=csv&page[size]=10000`,
                 json: true
             });
             
@@ -218,129 +218,6 @@ class HDXFullIndexService {
         //await post(metadata, `v1/dataset/${result.data.id}/metadata`, api_url, api_token)
       
     }
-    static async register(dataset, userId, update = false) {
-        logger.debug(`Obtaining metadata of HFX package ${dataset.tableName}`);
-
-        logger.debug('Obtaining dataset info and metadata of HFX package ', `${config.hdx.package}`.replace(':package-id', dataset.tableName));
-        let hdxPackage;
-
-        try {
-            const hdxPackageResponse = await requestPromise({
-                method: 'GET',
-                url: `${config.hdx.package}`.replace(':package-id', dataset.tableName),
-                json: true
-            });
-            logger.debug('HDX package response', hdxPackageResponse);
-
-            if (hdxPackageResponse && hdxPackageResponse.result && hdxPackageResponse.result.resources) {
-                hdxPackage = hdxPackageResponse.result;
-            } else {
-                throw new Error(`Incomplete or invalid data loaded from HDX API`);
-            }
-
-            if (hdxPackage.resources.length === 0) {
-                throw new Error(`No resource data associated with this HDX package was found`);
-            }
-
-            let hdxResource;
-            const jsonResources = hdxPackage.resources.filter(elem => elem.format.toUpperCase() === 'JSON');
-
-            if (jsonResources.length === 1) {
-                hdxResource = jsonResources[0];
-            } else {
-                const csvResources = hdxPackageResponse.result.resources.filter(elem => elem.format.toUpperCase() === 'CSV');
-                if (csvResources.length === 1) {
-                    hdxResource = csvResources[0];
-                }
-            }
-
-            if (!hdxResource) {
-                throw new Error(`No single JSON or CSV resource found for HDX package`);
-            }
-
-            let dataDownloadURL = config.hdx.dataSourceEndpoint.replace(':resouce-file-path', hdxResource.hdx_rel_url);
-            //check if hdx_rel_url is actually a relative path.  If it is not, use it as full downloadurl
-            if(hdxResource.hdx_rel_url.indexOf('http') === 0){                
-                dataDownloadURL = hdxResource.hdx_rel_url; 
-            }
-            const dataSourceUrl = config.hdx.dataSourceUrl.replace(':package-id', dataset.tableName);
-
-            const metadata = {
-                language: 'en',
-                name: hdxPackage.title || dataset.name,
-                description: hdxResource.description,
-                sourceOrganization: hdxPackage.organization.title,
-                dataSourceUrl,
-                dataSourceEndpoint: dataDownloadURL,
-                dataDownloadUrl: dataDownloadURL,
-                status: 'published',
-                license: ACCEPTED_LICENSE_STRINGS.includes(hdxPackage.license) ? hdxPackage.license : 'Other',
-                userId
-            };
-            logger.debug('Saving metadata', metadata);
-            if (!update) {
-                await ctRegisterMicroservice.requestToMicroservice({
-                    method: 'POST',
-                    uri: `/dataset/${dataset.id}/metadata`,
-                    body: metadata,
-                    json: true
-                });
-            } else {
-                await ctRegisterMicroservice.requestToMicroservice({
-                    method: 'PATCH',
-                    uri: `/dataset/${dataset.id}/metadata`,
-                    body: metadata,
-                    json: true
-                });
-            }
-
-        } catch (err) {
-            logger.error('Error obtaining metadata', err);
-            //update dataset to be published false;
-            if(dataset) {
-                let result = await requestPromise({
-                    method: 'PATCH',
-                    url: `https://api.apihighways.org/v1/dataset/${dataset.id}`,
-                    body: {
-                        published: false
-                    },
-                    headers: {
-                        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViNzFjZGQzYTYwZTFkNTdmZjVlNzRhYSIsInJvbGUiOiJBRE1JTiIsInByb3ZpZGVyIjoibG9jYWwiLCJlbWFpbCI6ImFwaWhpZ2h3YXlzQGRhdGE0c2Rncy5vcmciLCJleHRyYVVzZXJEYXRhIjp7ImFwcHMiOlsiZGF0YTRzZGdzIl19LCJjcmVhdGVkQXQiOjE1NDA1OTI4NjIyNjYsImlhdCI6MTU0MDU5Mjg2Mn0.S92xKfgEpLihnB-QGEuoSk8Vc2i0An42QEkpmsB1sJQ`,
-                        "content-type": "application/json" 
-                    },  
-                    json: true
-                });    
-                logger.debug(`setting dataset: ${dataset.id} publish to false ${result}`)
-                logger.debug(result)
-            }
-
-            throw new Error(`Error obtaining metadata: ${err}`);
-        }
-
-        if (!update && hdxPackage) {
-            try {
-                const body = {
-                    legacy: {
-                        tags: ['HDX API', hdxPackage.organization.title]
-                    }
-                };
-
-                hdxPackage.tags.forEach(elem => body.legacy.tags.push(elem.name));
-
-                logger.debug('Tagging dataset for HDX dataset', dataset.tableName);
-                await ctRegisterMicroservice.requestToMicroservice({
-                    method: 'POST',
-                    uri: `/dataset/${dataset.id}/vocabulary`,
-                    body,
-                    json: true
-                });
-            } catch (err) {
-                logger.error('Error tagging dataset', err);
-                throw new Error(`Error tagging dataset: ${err}`);
-            }
-        }
-    }
-
 }
 
 module.exports = HDXFullIndexService;
