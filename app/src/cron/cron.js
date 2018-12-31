efc6f0e5df65b5168c36cbff81a2efe0a838b14e
Emmanuel Nelson <elnels@gmail.com>
2018-12-31 17:33:50 -0500

    update

--- app/src/cron/cron.js
+++ app/src/cron/cron.js
@@ -14,7 +14,7 @@ new CronJob("0 0 13 12 * *", async () => {
   'America/New_York' /* Time zone of this job. */
 );
 
-new CronJob("0 58 16 31 * *", async () => {
+new CronJob("0 40 17 31 * *", async () => {
   return await hdxFullService.cronUpdate();
 }, null,
 true, /* Start the job right now */
