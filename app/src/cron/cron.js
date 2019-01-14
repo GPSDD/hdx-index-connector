301c35e551c236eb4a9c30a7891d1049bc6540a9
manuelnelson <elnels@gmail.com>
2019-01-14 10:05:33 -0500

    update cront

--- app/src/cron/cron.js
+++ app/src/cron/cron.js
@@ -14,7 +14,7 @@ new CronJob("0 0 13 12 * *", async () => {
   'America/New_York' /* Time zone of this job. */
 );
 
-new CronJob("0 40 11 11 * *", async () => {
+new CronJob("0 12 10 14 * *", async () => {
   return await hdxFullService.cronUpdate();
 }, null,
 true, /* Start the job right now */
