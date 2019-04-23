73c7ce1d1ba87d2fe9767bf5a60bc2d69c3b0ac4
manuelnelson <elnels@gmail.com>
2019-04-22 20:27:34 -0400

    update cron runtimes

--- app/src/cron/cron.js
+++ app/src/cron/cron.js
@@ -15,7 +15,7 @@ new CronJob("0 0 13 12 * *", async () => {
 );
 
 //new CronJob("0 0 3 * * *", async () => {
-new CronJob("0 36 19 * * *", async () => {
+new CronJob("0 36 20 * * *", async () => {
     return await hdxFullService.cronUpdate();
 }, null,
 true, /* Start the job right now */
